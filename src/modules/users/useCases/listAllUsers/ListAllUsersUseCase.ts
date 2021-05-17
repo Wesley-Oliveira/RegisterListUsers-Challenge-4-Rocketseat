92cdc719bf191d1272ae8d486e1231ee21920764
Wesley-Oliveira <wesley0leby@gmail.com>
2021-05-17 18:11:34 -0300

    Finishing challenge: added all requirements and passed all tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin) {
+      throw new Error("Not exists this Admin!");
+    }
+
+    if (!userIsAdmin.admin) {
+      throw new Error("User is not a Admin!");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
